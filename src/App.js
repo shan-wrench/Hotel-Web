408e111970167c86587ea57e963df57ccb0b0c48
Dilshan Nuwantha Wickramasingha <130473106+shan-wrench@users.noreply.github.com>
2024-10-09 22:50:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,14 +15,14 @@ export function App() {
   );
 }
 
-// import React from 'react';
+import React from 'react';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello, World!</h1>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
 
-// export default App; // Ensure you have this line
+export default App; // Ensure you have this line
