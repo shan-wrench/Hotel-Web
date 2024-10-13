9d5234bf72dbdf03b6377772d59f735ec67ab214
Dilshan Nuwantha Wickramasingha <130473106+shan-wrench@users.noreply.github.com>
2024-10-13 21:17:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import Navbar from "./common/Navbar/Navbar"
-// import Home from "./components/pages/Home"
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-// const App = () => {
-//   return (
-//     <>
-//       <Router>
-//         <Navbar/>
-//         <Routes>
-//           <Route path="/" element={<Home />} /> {/* Updated */}
-//         </Routes>
-//       </Router>
-//     </>
-//   )
-// }
-
-// export default App
-
 import React from 'react'
 import Navbar from "./common/Navbar/Navbar"
 import Home from "./components/pages/Home"
